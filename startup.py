dbb0f6f137102c75ccfadc17138ff270c2cec4e6
Brad <brad.solomon.1124@gmail.com>
2017-11-16 12:17:53 -0500

    Add files via upload

---
+++ startup.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# pandas options.  For full list and defaults:
+# https://pandas.pydata.org/pandas-docs/stable/options.html#available-options
+# Reference by full path here to avoid regex conflict with options
+#     added in the future.
+# Skipped here: 'html', 'io', & 'compute'.
+options = {
+    'display': {
+        'max_columns': None,
+        'max_rows': 25,
+        'max_seq_items': 50,
+        'precision': 3,
+        'show_dimensions': False
+        },
+    'mode': {
+        'chained_assignment': None
+        }
+    }
+
+for option in options:
+    for category, value in options[option].items():
+        pd.set_option('{0}.{1}'.format(option, category), value)
